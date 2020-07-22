14c09771bd9876faf469357e84dd6b017d649d76
Qasim Hassan <qhassan1213@gmail.com>
2020-07-22 16:27:10 +0500

    Updates

---
+++ precache-manifest.d2418cebc12f725569f69f90d35e06b1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d2ff135e6e57030a4a4b88dbfd13c8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "75ac2481b0bde64fb602",
+    "url": "/react-portfolio/static/css/3.db0b62c7.chunk.css"
+  },
+  {
+    "revision": "7c712c0e913fdbfd8116",
+    "url": "/react-portfolio/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "687f9dac4880fcd129f1",
+    "url": "/react-portfolio/static/css/main.8e0b1245.chunk.css"
+  },
+  {
+    "revision": "d8edf73814136210787a",
+    "url": "/react-portfolio/static/js/2.73ea0d0c.chunk.js"
+  },
+  {
+    "revision": "75ac2481b0bde64fb602",
+    "url": "/react-portfolio/static/js/3.24529e82.chunk.js"
+  },
+  {
+    "revision": "7c712c0e913fdbfd8116",
+    "url": "/react-portfolio/static/js/4.fdbd3849.chunk.js"
+  },
+  {
+    "revision": "687f9dac4880fcd129f1",
+    "url": "/react-portfolio/static/js/main.5bbd7b16.chunk.js"
+  },
+  {
+    "revision": "ce5aafc0abfe92069e35",
+    "url": "/react-portfolio/static/js/runtime-main.e4c20c21.js"
+  },
+  {
+    "revision": "dda8c45f7ac2ca2b6f6e27ea48379b27",
+    "url": "/react-portfolio/static/media/2842680new.dda8c45f.jpg"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/react-portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/react-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/react-portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/react-portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/react-portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/react-portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/react-portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/react-portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/react-portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/react-portfolio/static/media/talksCardBack.de3da806.svg"
+  }
+]);
